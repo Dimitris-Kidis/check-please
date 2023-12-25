59afba41a54f85e62e88fc026d74319a009f5bf3
Dmitrii <dmitrii.romanenco@amdaris.com>
2023-12-25 14:46:20 +0200

    #1.0: Backend Filtration History Done; Mapper Configuration Added

--- CheckPlease/Query/Repairs/GetRepairs/MapperProfile.cs
+++ CheckPlease/Query/Repairs/GetRepairs/MapperProfile.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using ApplicationCore.Domain.Entities;
+using AutoMapper;
+using OfficeOpenXml.Drawing.Chart;
+using Query.Cars.GetAllCars;
+using Query.Clients.GetClientHistory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,21 @@ using System.Threading.Tasks;
 
 namespace Query.Repairs.GetRepairs
 {
-    internal class MapperProfile
+    public class MapperProfile : Profile
     {
+        public MapperProfile()
+        {
+            CreateMap<GetAllCarsDto, GetAllCarsDto>();
+            CreateMap<IEnumerable<GetAllCarsDto>, IEnumerable<GetAllCarsDto>>();
+
+            CreateMap<Detail, DetailInfo>();
+
+            CreateMap<Repair, GetRepairsDto?>()
+                .ForMember(x => x.Id, opt => opt.MapFrom(x => x.Id))
+                .ForMember(x => x.Date, opt => opt.MapFrom(x => x.CreatedAt))
+                .ForMember(x => x.CarSign, opt => opt.MapFrom(x => x.Car.CarSign))
+                .ForMember(x => x.Mileage, opt => opt.MapFrom(x => x.Car.Mileage))
+                .ForMember(x => x.Details, opt => opt.MapFrom(x => x.Details));
+        }
     }
 }
